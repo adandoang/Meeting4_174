bc8d9dd1b242a0fb8006ff65abfe7361b7907121
adandoang <akhdanjzif@gmail.com>
2025-04-23 18:26:49 +0700

    membuat class loginpage

---
+++ android/app/src/main/kotlin/com/example/meet4project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.meet4project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/login.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
