4c6584941f48388522602d32a38ee62075921117
adandoang <akhdanjzif@gmail.com>
2025-04-23 19:58:01 +0700

    membuat kolom isi email dan password dalam halaman registerpage tidak lupa dengan button

--- lib/registerpage.dart
+++ lib/registerpage.dart
@@ -7,3 +7,52 @@ class Registerpage extends StatefulWidget {
   State<Registerpage> createState() => _RegisterPageState();
 }
 
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Register'),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(onPressed: () {}, child: Text('Register')),
+              TextButton(onPressed: () {
+                Navigator.pushNamed(context, '/login')
+              }, 
+              child: Text('Sudah punya akun? Login sekarang'))
+            ],
+          ),)),
+    )
+  }
+}
