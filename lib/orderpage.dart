3b0775f8a3b7cd815ee80ad3253d2c3a01a75775
adandoang <akhdanjzif@gmail.com>
2025-04-23 20:17:25 +0700

    membuat orderpagestate yang berisi controller dan fungsi perhitungan

--- lib/orderpage.dart
+++ lib/orderpage.dart
@@ -7,4 +7,19 @@ class OrderPage extends StatefulWidget {
   State<OrderPage> createState() => _OrderPageState();
 }
 
-class
+class _OrderPageState extends State<OrderPage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  final TextEditingController JumlahMakananController = TextEditingController();
+  final TextEditingController JumlahMinumannController = TextEditingController();
+  int totalHarga = 0;
+
+  void calculateTotalPrice() {
+    int jumlahMakanan = int.tryParse(JumlahMakananController.text) ?? 0;
+    int jumlahMinuman = int.tryParse(JumlahMinumannController.text) ?? 0;
+
+    setState(() {
+      totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
+    });
+  }
+}
